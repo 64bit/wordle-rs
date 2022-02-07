aa0148ec868ff39ad8606ea56abbec715cfa2d34
Himanshu Neema <himanshun.iitkgp@gmail.com>
2022-02-06 16:58:58 -0800

    readme and crate doc in lib.rs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Wordle
+# Wordle in Rust
 
 A Rust library and cli for [Wordle](https://www.powerlanguage.co.uk/wordle/).
 Inspired by [Wordle in Bash](https://gist.github.com/huytd/6a1a6a7b34a0d0abcac00b47e3d01513)
@@ -15,7 +15,27 @@ cargo install wordlers
 worlders
 ```
 
+![Play Demo](play-demo.gif)
 
 
+## Basic Usage
 
+```rust
+use wordlers::dictionary::EnglishDictionary;
+use wordlers::wordle::{Wordle, PlayResult};
 
+let dictionary = EnglishDictionary::new().unwrap();
+let mut wordle = Wordle::new(&dictionary);
+let play_result = wordle.play("dream");
+match play_result {
+  Ok(play_result) => {
+    println!("{}", play_result);
+    match play_result {
+        PlayResult::YouWon(_) => std::process::exit(0),
+        PlayResult::YouLost(_) => std::process::exit(1),
+        PlayResult::TurnResult(_) => {}
+    }
+  }
+  Err(e) => println!("{}", e),
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_doc_code_examples)]
+//! A library for Wordle and Dictionary to use with Wordle.
+//!
+//! # Play
+//!
+//! ```bash
+//! wordlers
+//! ```
+//!
+//! ![Play Demo](../../../play-demo.gif)
+//!
+//! # Example
+//!
+//! Basic usage:
+//!
+//! ```
+//! use wordlers::dictionary::EnglishDictionary;
+//! use wordlers::wordle::{Wordle, PlayResult};
+//!
+//! let dictionary = EnglishDictionary::new().unwrap();
+//! let mut wordle = Wordle::new(&dictionary);
+//! let play_result = wordle.play("dream");
+//! match play_result {
+//!   Ok(play_result) => {
+//!     println!("{}", play_result);
+//!     match play_result {
+//!         PlayResult::YouWon(_) => std::process::exit(0),
+//!         PlayResult::YouLost(_) => std::process::exit(1),
+//!         PlayResult::TurnResult(_) => {}
+//!     }
+//!   }
+//!   Err(e) => println!("{}", e),
+//! }
+//! ```
+
 pub mod dictionary;
-mod wordle;
-pub use wordle::*;
+pub mod wordle;
+
+// pub mod prelude {
+//     //! Make import easy
+//     pub use crate::wordle::*;
+//     pub use crate::dictionary::*;
+// }
